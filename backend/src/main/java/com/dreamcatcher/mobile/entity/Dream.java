198de3c270b67c55375a9a3feb7792d23828cd89
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-05-10 13:16:58 +0200

    Add Dream class to entity directory

---
+++ backend/src/main/java/com/dreamcatcher/mobile/entity/Dream.java
@@ -0,0 +1,85 @@
+package com.dreamcatcher.mobile.entity;
+
+public class Dream {
+    private Integer id;
+    private String name;
+    private String visitor;
+    private String plot;
+    private String location;
+    private String mood;
+
+    //Constructor
+    public Dream(){};
+
+    public Dream(Integer id, String name, String visitor, String plot, String location, String mood){
+        this.id = id;
+        this.name = name;
+        this.visitor = visitor;
+        this.plot = plot;
+        this.location = location;
+        this.mood = mood;
+    }
+
+    //Getters
+    public Integer getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getVisitor(){
+        return visitor;
+    }
+
+    public String getPlot(){
+        return plot;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    public String getMood(){
+        return mood;
+    }
+
+    //Setters
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVisitor(String visitor) {
+        this.visitor = visitor;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    //toString
+    @Override
+    public String toString(){
+        return "Dream{" +
+            "id=" + id +
+            ", name=" + name +
+            ", visitor=" + visitor +
+            ", plot=" + plot +
+            ", location=" + location +
+            ", mood=" + mood +
+            "}";
+    }
+}
