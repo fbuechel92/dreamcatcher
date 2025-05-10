0e87f15886bec68a58f2761d53b49d94d854d8eb
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-05-10 12:14:20 +0200

    Add backend folder with spring project and frontend folder

---
+++ backend/src/test/java/com/dreamcatcher/mobile/MobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamcatcher.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
