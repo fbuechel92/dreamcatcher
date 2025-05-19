29f37ddd9d6153d081306d2965c2280e924ca417
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-05-19 21:45:56 +0200

    Create dream analysis and dto class

---
+++ backend/src/main/java/com/dreamcatcher/mobile/entity/DreamAnalysis.java
@@ -0,0 +1,96 @@
+package com.dreamcatcher.mobile.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class DreamAnalysis {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer dreamAnalysisId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    private String dreamTitle;
+    private String dreamTheme;
+    private String interpretation;
+    private String implications;
+
+    // Constructor
+    public DreamAnalysis() {}
+
+    public DreamAnalysis(Integer dreamAnalysisId, User user, String dreamTitle, String dreamTheme, String interpretation, String implications) {
+        this.dreamAnalysisId = dreamAnalysisId;
+        this.user = user;
+        this.dreamTitle = dreamTitle;
+        this.dreamTheme = dreamTheme;
+        this.interpretation = interpretation;
+        this.implications = implications;
+    }
+
+    // Getters
+    public Integer getPointAnalysisId() {
+        return dreamAnalysisId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getDreamTitle() {
+        return dreamTitle;
+    }
+
+    public String getDreamTheme() {
+        return dreamTheme;
+    }
+
+    public String getInterpretation() {
+        return interpretation;
+    }
+
+    public String getImplications() {
+        return implications;
+    }
+
+    // Setters
+    public void setPointAnalysisId(Integer pointAnalysisId) {
+        this.dreamAnalysisId = pointAnalysisId;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setDreamTitle(String dreamTitle) {
+        this.dreamTitle = dreamTitle;
+    }
+
+    public void setDreamTheme(String dreamTheme) {
+        this.dreamTheme = dreamTheme;
+    }
+
+    public void setInterpretation(String interpretation) {
+        this.interpretation = interpretation;
+    }
+
+    public void setImplications(String implications) {
+        this.implications = implications;
+    }
+
+    @Override
+    public String toString() {
+        return "PointAnalysis{" +
+                "pointAnalysisId=" + dreamAnalysisId +
+                ", user=" + user +
+                ", dreamTitle=" + dreamTitle +
+                ", dreamTheme=" + dreamTheme +
+                ", interpretation=" + interpretation +
+                ", implications=" + implications +
+                "}";
+    }
+}
