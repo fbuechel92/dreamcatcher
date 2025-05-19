282fbd0707c2ba6f9d6d419846bb8f1fe333a273
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-05-19 18:54:53 +0200

    Adjust dream dto with additional class attributes from Dream

--- backend/src/main/java/com/dreamcatcher/mobile/dto/Dream.java
+++ backend/src/main/java/com/dreamcatcher/mobile/dto/Dream.java
@@ -1,10 +1,14 @@
 package com.dreamcatcher.mobile.dto;
 
 public record Dream(
-    Integer id,
-    String name,
+    Integer dreamId,
+    Integer userId,
+    Integer analysisId,
+    Integer sleepId,
+    Integer theoryId,
     String visitor,
     String plot,
     String location,
-    String mood
+    String mood,
+    String additionalInfo
 ) {}
