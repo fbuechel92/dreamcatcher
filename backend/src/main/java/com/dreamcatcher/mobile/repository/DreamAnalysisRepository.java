830c64661c1bff8f5053b845cd38b49e5f027ca3
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-05-20 20:33:35 +0200

    Add repos

---
+++ backend/src/main/java/com/dreamcatcher/mobile/repository/DreamAnalysisRepository.java
@@ -0,0 +1,8 @@
+package com.dreamcatcher.mobile.repository;
+
+import com.dreamcatcher.mobile.entity.DreamAnalysis;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DreamAnalysisRepository extends JpaRepository<DreamAnalysis, Integer> {
+}
+
---
+++ backend/src/main/java/com/dreamcatcher/mobile/repository/TheoryRepository.java
@@ -0,0 +1,8 @@
+package com.dreamcatcher.mobile.repository;
+
+import com.dreamcatcher.mobile.entity.Theory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TheoryRepository extends JpaRepository<Theory, Integer> {
+}
+
