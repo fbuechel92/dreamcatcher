628dc00de56a40317c15ee151efa78e09318ff5f
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-05-20 22:18:28 +0200

    Create tentative schema for the tables

--- backend/src/main/java/com/example/model/User.java
+++
@@ -1,8 +0,0 @@
-public class User {
-    // ...existing code...
-    private Long id;
-    private String username;
-    private String email;
-    private String password;
-    // ...existing code...
-}
