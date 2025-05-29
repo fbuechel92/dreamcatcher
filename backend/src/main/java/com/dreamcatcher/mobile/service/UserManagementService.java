3428be0c577325b17e1fb99fc110770785b09d66
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-05-29 16:12:59 +0200

    Add new change method

--- backend/src/main/java/com/dreamcatcher/mobile/service/UserManagementService.java
+++ backend/src/main/java/com/dreamcatcher/mobile/service/UserManagementService.java
@@ -6,6 +6,7 @@ import com.dreamcatcher.mobile.entity.User;
 import com.dreamcatcher.mobile.repository.UserRepository;
 import com.dreamcatcher.mobile.utilities.UserDTOMapper;
 import com.dreamcatcher.mobile.utilities.UserEntityMapper;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -47,13 +48,64 @@ public class UserManagementService {
     }
 
     //Method to provide user information if user visits user profile
-    public UserAccountDTO getUserData(Integer userId){
+    public UserAccountDTO getUser(Integer userId){
         try {
             User user = userRepository.findById(userId).orElseThrow();
             UserAccountDTO userAccountDTO = userDTOMapper.mapToUserAccountDTO(user);
             return userAccountDTO;
         } catch (Exception e) {
             throw new IllegalArgumentException("User with ID " + userId + " not found");
+        }
+    }
+
+    //Method to change user data
+    public User modifyUser(Integer userId, UserAccountDTO userAccountDTO){
+        //Retrieving user by id from repo and converting dto user to entity user
+        try {
+            User currentUser = userRepository.findById(userId).orElseThrow();
+            User submittedUser = userEntityMapper.mapToUserAccountEntity(userAccountDTO);
+            Integer changeCounter = 0;
+
+            //Check what data changed
+            if(!currentUser.getEmail().equals(submittedUser.getEmail())){
+                currentUser.setEmail(submittedUser.getEmail());
+                changeCounter += 1;
+            }
+
+            if(!currentUser.getName().equals(submittedUser.getName())){
+                currentUser.setName(submittedUser.getName());
+                changeCounter += 1;
+            }
+
+            if(!currentUser.getGender().equals(submittedUser.getGender())){
+                currentUser.setGender(submittedUser.getGender());
+                changeCounter += 1;
+            }
+
+            if(!currentUser.getBirthdate().equals(submittedUser.getBirthdate())){
+                currentUser.setBirthdate(submittedUser.getBirthdate());
+                changeCounter += 1;
+            }
+
+            if(!currentUser.getCountry().equals(submittedUser.getCountry())){
+                currentUser.setCountry(submittedUser.getCountry());
+                changeCounter += 1;
+            }
+
+            if(!currentUser.getOccupation().equals(submittedUser.getOccupation())){
+                currentUser.setOccupation(submittedUser.getOccupation());
+                changeCounter += 1;
+            }
+
+            if(changeCounter>0){
+                return userRepository.save(currentUser);
+            } else {
+                System.out.println("No change detected");
+                return currentUser;
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("We had trouble saving this :-(");
         }
     }
 }
--- backend/src/main/java/com/dreamcatcher/mobile/utilities/UserEntityMapper.java
+++ backend/src/main/java/com/dreamcatcher/mobile/utilities/UserEntityMapper.java
@@ -1,7 +1,9 @@
 package com.dreamcatcher.mobile.utilities;
 
 import com.dreamcatcher.mobile.dto.UserAccountCreationDTO;
+import com.dreamcatcher.mobile.dto.UserAccountDTO;
 import com.dreamcatcher.mobile.entity.User;
+import java.util.Date;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +14,17 @@ public class UserEntityMapper {
             userAccountCreationDTO.email(),
             userAccountCreationDTO.password(),
             userAccountCreationDTO.name()
+        );
+    }
+
+    public User mapToUserAccountEntity(UserAccountDTO userAccountDTO) {
+        return new User(
+            userAccountDTO.email(),
+            userAccountDTO.name(),
+            userAccountDTO.gender(),
+            userAccountDTO.birthdate(),
+            userAccountDTO.country(),
+            userAccountDTO.occupation()
         );
     }
 }
