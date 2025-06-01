55db53870f83a5795fae485b67a9ba3f6f5a4e67
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-06-01 15:00:15 +0200

    Add reflection updater

---
+++ backend/src/main/java/com/dreamcatcher/mobile/utilities/ReflectionUpdater.java
@@ -0,0 +1,44 @@
+package com.dreamcatcher.mobile.utilities;
+
+import com.dreamcatcher.mobile.entity.User;
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+public class ReflectionUpdater {
+
+    //Method identifies getter and setter methods, then invokes the setter IF there was a change
+    public boolean updateFields(User currentUser, User submittedUser){
+
+        String fieldName;
+        String setterName;
+        boolean changeDetected = false;
+
+        try{
+            //Identify field names
+            Method[] methods = currentUser.getClass().getMethods();
+
+            for(Method method:methods){
+                if(method.getName().substring(0,3).equals("get")){
+
+                    //Determine getter and setter
+                    fieldName = method.getName().substring(3);
+                    setterName = "set" + fieldName;
+                    Method setter = currentUser.getClass().getMethod(setterName, method.getReturnType());
+                    Method getter = method;
+
+                    //Compare values
+                    Object currentValue = getter.invoke(currentUser);
+                    Object submittedValue = getter.invoke(submittedUser);
+
+                    if(!Objects.equals(currentValue, submittedValue)){
+                        setter.invoke(currentUser, submittedValue);
+                        changeDetected = true;
+                    }
+                }
+            }
+            return changeDetected;
+        } catch (Exception e) {
+            throw new RuntimeException("We had trouble saving this :-(");
+        }
+    }
+}
