92a2b4e1dfc43fcc06c2d6f7b0a12c6931329feb
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-06-10 19:40:46 +0200

    Add createUser controller method

--- backend/src/main/java/com/dreamcatcher/mobile/controller/UserController.java
+++ backend/src/main/java/com/dreamcatcher/mobile/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.dreamcatcher.mobile.controller;
 
+import com.dreamcatcher.mobile.dto.UserAccountCreationDTO;
 import com.dreamcatcher.mobile.dto.UserAccountDTO;
+import com.dreamcatcher.mobile.entity.User;
 import com.dreamcatcher.mobile.service.UserManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -24,5 +27,11 @@ public class UserController {
     public ResponseEntity<UserAccountDTO> getUser(@PathVariable Integer userId){
         UserAccountDTO userAccountDTO = userManagementService.getUser(userId);
         return ResponseEntity.status(HttpStatus.OK).body(userAccountDTO);
+    }
+
+    @PostMapping("/profile")
+    public ResponseEntity<User> createUser(UserAccountCreationDTO userAccountCreationDTO){
+        User user = userManagementService.createUser(userAccountCreationDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
