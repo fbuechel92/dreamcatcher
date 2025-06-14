43928338c203006746f2bc3d9346d03ed0c7c213
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-06-14 21:51:38 +0200

    Change testint set up

--- backend/src/test/java/com/dreamcatcher/mobile/unitTest/UserManagementServiceUnitTest.java
+++ backend/src/test/java/com/dreamcatcher/mobile/unitTest/UserManagementServiceUnitTest.java
@@ -1,13 +1,15 @@
 package com.dreamcatcher.mobile.unitTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-import com.dreamcatcher.mobile.dto.UserAccountCreationDTO;
+import com.dreamcatcher.mobile.dto.UserAuthDTO;
+import com.dreamcatcher.mobile.dto.UserProfileDTO;
 import com.dreamcatcher.mobile.entity.User;
 import com.dreamcatcher.mobile.repository.UserRepository;
 import com.dreamcatcher.mobile.service.UserManagementService;
-import com.dreamcatcher.mobile.utilities.ReflectionUpdater;
+import com.dreamcatcher.mobile.utilities.UserUpdater;
 import com.dreamcatcher.mobile.utilities.UserDTOMapper;
 import com.dreamcatcher.mobile.utilities.UserEntityMapper;
 import java.time.LocalDate;
@@ -15,70 +17,30 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class UserManagementServiceUnitTest {
 
-    @Mock
-    private UserRepository userRepository;
-
-    @Mock
-    private UserEntityMapper userEntityMapper;
-
-    @Mock
-    private UserDTOMapper userDTOMapper;
-
-    @Mock
-    private BCryptPasswordEncoder passwordEncoder;
-
-    @Mock
-    private ReflectionUpdater reflectionUpdate;
-
-    @InjectMocks
-    private UserManagementService userManagementService;
+    private BCryptPasswordEncoder passwordEncoder = Mockito.mock(BCryptPasswordEncoder.class);
 
     @Test
-    public void testDuplicateEmailThrowsException(){
+    public void testModifyAuthWithoutDataChange(){
 
-        //mock method
-        when(userRepository.existsByEmail("test@gmail.com")).thenReturn(true);
+        String hashedPassword = "$2a$12$65Fi4l1I7iUW3Vy7fAb2P.UmcLlT5vSF0In7.y6bGQk01oy1XLiJK";
 
-        //create dto
-        UserAccountCreationDTO dto = new UserAccountCreationDTO("test@gmail.com", "1234", "Heisenberg");
-
-        // Assert exception
-        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            userManagementService.createUser(dto);
-        });
-
-        // Verify exception message
-        assertEquals("Email already exists. Please use another email address.", exception.getMessage());
-    }
-
-    @Test
-    public void testDifferingUserData(){
         //Create User
-        User currentUser = new User("test@gmail.com", "Heisenberg", "male", LocalDate.parse("1901-12-05"), "Germany", "Physicist");
-        User submittedUser = new User("test@gmail.com", "Curie", "female", LocalDate.parse("1934-07-04"), "Germany", "Physicist");
+        User currentUser = new User("test@gmail.com", hashedPassword, "Curie");
+        User submittedUser = new User("test@gmail.com", "password123","Curie");
+
+        //mock password encoder
+        when(passwordEncoder.encode(anyString())).thenReturn(hashedPassword);
 
         //Apply change to current user and check if change was made
-        ReflectionUpdater reflectionUpdater = new ReflectionUpdater();
-        boolean userAppliedChange = reflectionUpdater.updateFields(currentUser, submittedUser);
+        UserUpdater userUpdater = new UserUpdater(passwordEncoder);
+        boolean userAppliedChange = userUpdater.updateAuthFields(currentUser, submittedUser);
 
-        Assertions.assertTrue(userAppliedChange, "The result of userAppliedChange should be true but it's false");
-        Assertions.assertEquals("Curie", currentUser.getName(),"The user name should have changed to Curie but it didn't");
-    }
-
-    @Test
-    public void testSameUserData(){
-        //Create User
-        User currentUser = new User("test@gmail.com", "Heisenberg", "male", LocalDate.parse("1901-12-05"), "Germany", "Physicist");
-        User submittedUser = new User("test@gmail.com", "Heisenberg", "male", LocalDate.parse("1901-12-05"), "Germany", "Physicist");
-
-        //Apply change to current user and check if change was made
-        ReflectionUpdater reflectionUpdater = new ReflectionUpdater();
-        boolean userAppliedChange = reflectionUpdater.updateFields(currentUser, submittedUser);
-
-        Assertions.assertFalse(userAppliedChange, "The result of userAppliedChange should be false but it's true");
+        Assertions.assertFalse(userAppliedChange, "There was no data change but updateAuthFields returns True.");
+        Assertions.assertEquals("Curie", currentUser.getName(),"The user name should have stayed the same but it changed.");
     }
 }
