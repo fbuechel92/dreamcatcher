a59344ca31cd1455a58ef2ceb08816663555cfa1
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-06-19 20:36:24 +0200

    Add dream entity mapper to create dream class'

--- backend/src/main/java/com/dreamcatcher/mobile/service/DreamManagementService.java
+++ backend/src/main/java/com/dreamcatcher/mobile/service/DreamManagementService.java
@@ -2,18 +2,23 @@ package com.dreamcatcher.mobile.service;
 
 import com.dreamcatcher.mobile.dto.DreamDTO;
 import com.dreamcatcher.mobile.entity.Dream;
+import com.dreamcatcher.mobile.mapper.DreamEntityMapper;
 import com.dreamcatcher.mobile.repository.DreamRepository;
 
 public class DreamManagementService {
 
     private DreamRepository dreamRepository;
+    private DreamEntityMapper dreamEntityMapper;
 
-    public DreamManagementService(DreamRepository dreamRepository){
+    public DreamManagementService(DreamRepository dreamRepository, DreamEntityMapper dreamEntityMapper){
         this.dreamRepository = dreamRepository;
+        this.dreamEntityMapper = dreamEntityMapper;
     }
 
     //Create Dream Method
     public Dream createDream(DreamDTO dreamDTO){
+
+        DreamEntityMapper dreamEntityMapper;
 
         //Translate DTO to entity
         Dream dream = DreamEntityMapper.mapToDreamEntity(dreamDTO);
