67bbeba22130f5e2a9381deaeb3c174222088388
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-07-22 21:34:34 +0200

    Bilding structure of dream screen

--- client/screens/DreamScreen.tsx
+++ client/screens/DreamScreen.tsx
@@ -3,11 +3,25 @@ import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function DreamScreen() {
     return(
-        <View>
-            <Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>
                 Let's catch your dream!
             </Text>
         </View>
 
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize:20,
+        fontWeight: 'bold',
+        color: '#8A2BE2',
+        fontFamily: 'Bradley Hand'
+    }
+});
