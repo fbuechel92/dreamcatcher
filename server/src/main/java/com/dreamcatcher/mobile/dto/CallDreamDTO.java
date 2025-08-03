b15ea1121c7aa5d47c545c562b869bbe1bec3ad4
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-08-03 09:27:17 +0200

    Change dto to accommodate enums

--- server/src/main/java/com/dreamcatcher/mobile/dto/CallDreamDTO.java
+++ server/src/main/java/com/dreamcatcher/mobile/dto/CallDreamDTO.java
@@ -2,13 +2,16 @@ package com.dreamcatcher.mobile.dto;
 
 import java.time.LocalDateTime;
 
+import com.dreamcatcher.mobile.enums.SleepQuality;
+import com.dreamcatcher.mobile.enums.Mood;
+
 public record CallDreamDTO(
     Integer dreamId,
     String visitor,
     String plot,
     String location,
-    String mood,
-    String sleepQuality,
+    Mood mood,
+    SleepQuality sleepQuality,
     String additionalInfo,
     LocalDateTime createdAt,
     LocalDateTime updatedAt
--- server/src/main/java/com/dreamcatcher/mobile/dto/SubmitDreamDTO.java
+++ server/src/main/java/com/dreamcatcher/mobile/dto/SubmitDreamDTO.java
@@ -1,11 +1,14 @@
 package com.dreamcatcher.mobile.dto;
 
+import com.dreamcatcher.mobile.enums.Mood;
+import com.dreamcatcher.mobile.enums.SleepQuality;
+
 public record SubmitDreamDTO(
     Integer dreamId,
     String visitor,
     String plot,
     String location,
-    String mood,
-    String sleepQuality,
+    Mood mood,
+    SleepQuality sleepQuality,
     String additionalInfo
 ) {}
