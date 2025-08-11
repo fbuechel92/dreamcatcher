9222ddf87fe4c29d49bdba98e24d00fdb47bd620
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-08-11 18:44:52 +0200

    Add auth0 file

---
+++ client/services/auth0.ts
@@ -0,0 +1,8 @@
+import Auth0 from 'react-native-auth0';
+
+const auth0 = new Auth0({
+  domain: 'dev-rg7an3n72l8syk8o.eu.auth0.com',
+  clientId: 'BOkgb4ooNuHI05g6poQN4Zkzw48BxRFZ',
+});
+
+export default auth0;
