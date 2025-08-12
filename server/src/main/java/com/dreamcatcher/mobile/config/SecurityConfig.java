b6c8dd4f1ff934c1a1fa3a295f75cef201120ec7
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-08-12 21:33:26 +0200

    Overwrite security hash config with security filter chain

--- server/src/main/java/com/dreamcatcher/mobile/config/SecurityConfig.java
+++ server/src/main/java/com/dreamcatcher/mobile/config/SecurityConfig.java
@@ -2,12 +2,22 @@ package com.dreamcatcher.mobile.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/public/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> {})
+            );
+        return http.build();
     }
 }
