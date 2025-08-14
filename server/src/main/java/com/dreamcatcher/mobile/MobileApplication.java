8cee2b240d74d1b8bd07c6fdd364505796edbb1b
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-08-14 21:23:12 +0200

    remove unit tests and security exclusion

--- server/src/main/java/com/dreamcatcher/mobile/MobileApplication.java
+++ server/src/main/java/com/dreamcatcher/mobile/MobileApplication.java
@@ -3,7 +3,7 @@ package com.dreamcatcher.mobile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class MobileApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MobileApplication.class, args);
