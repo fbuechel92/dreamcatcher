45d7c93f09b26462d35c07af9b657e4c0a5781f0
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-08-17 13:41:36 +0200

    Add context to the app file

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import WelcomeScreen from "../screens/WelcomeScreen";
 import MainScreen from "../screens/MainScreen";
+import { AuthProvider } from '../contexts/AuthContext';
 
 export default function App() {
+  
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
   const handleLoginSuccess = () => {
@@ -12,8 +14,13 @@ export default function App() {
   const handleLogout = () => {
     setIsLoggedIn(false);
   };
-
-  return isLoggedIn ? 
-    <MainScreen onLogout={handleLogout} /> : 
-    <WelcomeScreen onLoginSuccess={handleLoginSuccess} />;
+  
+  return(
+    <AuthProvider>
+      {isLoggedIn ? 
+        <MainScreen onLogout={handleLogout} /> : 
+        <WelcomeScreen onLoginSuccess={handleLoginSuccess} />
+      }
+    </AuthProvider>
+  );
 }
