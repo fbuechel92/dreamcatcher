38d2aecb0bff884de762034187c499da675daa07
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-09-13 10:57:25 +0200

    New redirect logic using token

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,6 +1,7 @@
 import WelcomeScreen from "../screens/WelcomeScreen";
 import MainScreen from "../screens/MainScreen";
 import React from "react";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
--- client/components/AuthButton.tsx
+++ client/components/AuthButton.tsx
@@ -15,7 +15,7 @@ interface User {
 }
 
 interface AuthButtonProps {
-  onLoginSuccess?: () => void;
+  onLoginSuccess?: (token: string) => void;
   onLogout?: () => void;
 }
 
@@ -89,7 +89,7 @@ export default function AuthButton({ onLoginSuccess, onLogout }: AuthButtonProps
           .catch(error => console.error('Error checking/creating user:', error));
           
             
-          onLoginSuccess?.();
+          onLoginSuccess?.(result.params.access_token);
         })
         .catch(error => {
           console.error('User info error:', error);
