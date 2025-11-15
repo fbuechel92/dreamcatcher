2ecf8f290adfb38d250a56a26126b1c9dcda6bb0
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-11-15 11:36:01 +0100

    adjust dream analysis dto to contain some more useful fields like theory name

--- server/src/main/java/com/dreamcatcher/mobile/dto/DreamAnalysisDTO.java
+++ server/src/main/java/com/dreamcatcher/mobile/dto/DreamAnalysisDTO.java
@@ -1,11 +1,16 @@
 package com.dreamcatcher.mobile.dto;
 
+import java.time.LocalDateTime;
+
 public record DreamAnalysisDTO(
     Integer dreamAnalysisId,
     Integer userId,
     Integer theoryId,
+    String theoryName,
     String dreamTitle,
     String dreamTheme,
     String interpretation,
-    String implications
+    String implications,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt
 ) {}
