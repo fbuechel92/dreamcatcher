e3450abb7c9f583292ea9f279b835f8b7eb14022
Fabian Bücheler <fabianbuecheler@gmx.de>
2025-11-17 20:11:16 +0100

    changes to ollama service

--- server/src/main/java/com/dreamcatcher/mobile/service/OllamaClient.java
+++ server/src/main/java/com/dreamcatcher/mobile/service/OllamaClient.java
@@ -8,10 +8,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.apache.hc.client5.http.config.RequestConfig; // Correct import
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient; // Correct import
-import org.apache.hc.client5.http.impl.classic.HttpClients; // Correct import
-import org.apache.hc.core5.util.Timeout; // Correct import for Timeout
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.util.Timeout;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
